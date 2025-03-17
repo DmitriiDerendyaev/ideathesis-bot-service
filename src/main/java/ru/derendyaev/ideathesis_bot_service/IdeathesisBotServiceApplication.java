d400d4723dfe7614c581d1376985f2d07b61b70a
Dmitrii Derendyaev <dds_stalker@mail.ru>
2025-03-17 21:35:05 +0400

    first commit

---
+++ src/main/java/ru/derendyaev/ideathesis_bot_service/IdeathesisBotServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.derendyaev.ideathesis_bot_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeathesisBotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeathesisBotServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/derendyaev/ideathesis_bot_service/IdeathesisBotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.derendyaev.ideathesis_bot_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeathesisBotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
